136381abd45237ed29daf4d10d2e41b7ce526509
Federico 'Magus' Rocca <frocca17@gmail.com>
2020-01-15 23:15:28 -0300

    Sección 1: Fundamentos Java: Temario General y Recursos del Curso Sección 2: Fundamentos Java: Introducción a Java Sección 3: Fundamentos Java: Instalación de JDK de Java y Herramientas Sección 4: Fundamentos Java: Variables en Java

---
+++ Sección 3/Ejercicio9/src/main/java/HolaMundo.java
@@ -0,0 +1,16 @@
+
+public class HolaMundo
+{
+
+    /*public static void main(String args[])
+    {
+        System.out.println("Hola mundo!");
+    }*/
+
+    // psvm
+    public static void main(String[] args)
+    {
+        // sout
+        System.out.println("Hola mundo!");
+    }
+}
---
+++ Sección 4/Ejercicio12/src/main/java/Ejercicio12.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Ejercicio12 
+{
+    public static void main(String[] args) 
+    {
+    // Ejercicio 12
+    /*
+        String mensajeSaludo = "Hola mundo!";
+        System.out.println(mensajeSaludo);
+
+        var mensajeDespedida = "Chau mundo!";
+        System.out.println(mensajeDespedida);
+
+        var numero = 1;
+        System.out.println(numero);
+    */
+    
+    // Ejercicio 13
+    /*
+        var usuario = "Juan";
+        var saludar = "Hola";
+        
+        System.out.println(saludar + " " + usuario);
+        
+        int i = 3;
+        int j = 5;
+        System.out.println(i + j);
+        
+        System.out.println(saludar + i + j);
+        
+        System.out.println(i + j + saludar);
+    */
+        
+    // Ejercicio 14
+    /*
+        String saludo = "Hola";
+        System.out.println(saludo);
+        
+        String variableLargaConVariosNombres;
+        int _hola;
+        float $adios;
+        char adios;
+    */
+        
+    // Ejercicio 15
+    /*
+        String nombre = "Karla";
+        String apellido = "Esparza";
+        System.out.println(nombre + " " + apellido);
+        System.out.println("Nueva linea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b" + nombre);
+        System.out.println("Retorno de carro: \r" + nombre);
+        System.out.println("Comilla simple: \'" + nombre + "\'");
+        System.out.println("Comilla simple: '" + nombre + "'");
+        System.out.println("Comilla doble: \"" + nombre + "\"");
+        System.out.println("saludos");
+        System.out.print("adios");
+        System.out.println("nos vemos");
+    */
+        
+    //Ejercicio 16
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresá el nombre del usuario: ");
+        var usuario = scanner.nextLine();
+        System.out.println("Usuario: " + usuario);
+        
+    }
+}
