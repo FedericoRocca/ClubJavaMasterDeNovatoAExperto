2d1979fc141d54be51a9ae48ebf4e243c63d4917
Federico 'Magus' Rocca <frocca17@gmail.com>
2020-01-27 22:47:38 -0300

    50. Ejercicio: Creación de Métodos en Java 52. Ejercicio: Constructores en Java

---
+++ ProyectoAritmetica/src/main/java/Aritmetica.java
@@ -0,0 +1,12 @@
+public class Aritmetica 
+{
+    public int sumar(int valA, int valB)
+    {
+        return valA + valB;
+    }
+    
+    public Aritmetica()
+    {
+        System.out.println("Se ejecuta el constructor vacio");
+    }
+}
---
+++ ProyectoAritmetica/src/main/java/PruebaAritmetica.java
@@ -0,0 +1,8 @@
+public class PruebaAritmetica 
+{
+    public static void main(String[] args)
+    {
+        Aritmetica aritmeticaObject = new Aritmetica();
+        System.out.println(aritmeticaObject.sumar(2, 2));
+    }
+}
